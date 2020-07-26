345742c5f835708a5d5da87d90d45169d27a15b7
LouisEiermann <louis.eiermann@gmail.com>
2020-07-26 12:39:34 +0200

    fixed devtool integration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import multi from 'redux-multi';
 import allReducers from './reducers';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = composeWithDevTools({
+  // options like actionSanitizer, stateSanitizer
+});
 
 export const store = createStore(
   allReducers,
-  compose(
-  applyMiddleware(multi, thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  composeEnhancers(
+  applyMiddleware(multi, thunk)
+  )
 );
 
 ReactDOM.render(
